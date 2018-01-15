85f7f06d5472e11e7e95a0958ea1559aa543fbde
HaoquanShen <shenhaoquan422@163.com>
2018-01-16 00:25:06 +0800

    data saving procedure.

--- api/zb_market.py
+++ api/zb_market.py
@@ -1,6 +1,9 @@
-import json, urllib2,time
+import json,time
+import urllib   
+import urllib2 
 import sys
 import logging
+import numpy as np
 
 logging.basicConfig(level=logging.DEBUG,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
@@ -12,14 +15,15 @@ class zb_market:
     def __init__(self,url,market = "btc_usdt"):
         self.url = url
         self.url = "http://api.zb.com/data/v1/"
-        self.market = "market="+market
+        self.market = market
         pass
 
-    def process_request(self,url):
-        try:
-            request = urllib2.Request(url)
-            response = urllib2.urlopen(request, timeout=2)
-            doc = json.loads(response.read())
+    def process_request(self,url,values):
+        try:  
+            data = urllib.urlencode(values)
+            req = urllib2.Request(url, data)
+            response = urllib2.urlopen(req)
+            doc = response.read()
             return doc
         except Exception, ex:
             print >> sys.stderr, 'zb request ex: ', ex
@@ -27,35 +31,44 @@ class zb_market:
 
     def get_market(self):
         reqTime = (int)(time.time() * 1000)
-
         url = self.url + "markets"
         doc = self.process_request(url)
-        print doc
-        return doc
+        try:
+            detail = doc[self.market]
+            amountScale = detail['amountScale']
+            priceScale = detail['priceScale']
+        except:
+            print 'Parse market error!'
+        return amountScale, priceScale
 
 
     def get_tick(self):
         reqTime = (int)(time.time() * 1000)
-        url = self.url + "ticker" + "?" + self.market
+        url = self.url + "ticker" + "?market=" + self.market
+        print url
         doc = self.process_request(url)
         return doc
 
     def get_depth(self,depth_size):
         reqTime = (int)(time.time() * 1000)
         depth_str = "size=" + str(depth_size)
-        url = self.url + "depth" + "?" + self.market+depth_str
+        url = self.url + "depth" + "?market=" + self.market+depth_str
         doc = self.process_request(url)
         return doc
 
-    def get_trade(self):
-        url = self.url + "trades"+"?" + self.market
-        print url
-        doc = self.process_request(url)
-        return doc
+    def get_trade(self, tid):
+        url = self.url + "trades"
+        if tid == None:
+            values = {'market' : self.market} 
+        else:
+            values = {'market' : self.market, 'since' : tid} 
+        doc = self.process_request(url, values)
+        return eval(doc)
 
     def get_kline(self):
-        url = self.url + "kline" + "?" + self.market
-        print url
+        url = self.url + "kline" + "?market=" + self.market + '&type=1min'
         doc = self.process_request(url)
+        print eval(doc)['moneyType']
+        print np.array(eval(doc)['data'])
         return doc
 
--- main.py
+++ main.py
@@ -2,24 +2,48 @@ from api import zb_api as zba
 from api import zb_market as zbm
 import sys
 import os
+import time
 
 if __name__ == '__main__':
 
     with open("config.txt","r") as f:
         access_key = f.readline().strip()
         access_secret = f.readline().strip()
-    print "access_key:",access_key
-    print "access_secret:", access_secret
+    # print "access_key:",access_key
+    # print "access_secret:", access_secret
+
+
+    zb_market = zbm.zb_market("", "btc_usdt")
+    trades = list()
+    tid = None
+    seep_time = 1.0
+    while (True):
+        trade_delta = zb_market.get_trade(tid)
+        num = len(trade_delta)
+        tid = trade_delta[num-1]['tid']
+        if num>25 and seep_time*0.9>0.1:
+            seep_time = seep_time*0.9
+        if num<5 and seep_time<10:
+            seep_time = seep_time*1.1
+        time.sleep(seep_time)
+        if num>1:
+            trade_delta = trade_delta[1::]
+            trades.append(trade_delta)
+        print num, seep_time
+
+    print 'done'
+    sys.exit()
+    # zb_market.get_tick()
+    # zb_market.get_depth(3)
+    zb_market.get_kline()
+
+
+    # amountScale, priceScale = zb_market.get_market()
+    # print amountScale, priceScale
+
 
     zb_api = zba.zb_api(access_key, access_secret,"btc")
     zb_api.query_account()
     zb_api.get_useraddress()
     zb_api.get_order()
     #test zb_market
-    sys.exit()
-    zb_market = zbm.zb_market("")
-    zb_market.get_market()
-    zb_market.get_tick()
-    zb_market.get_depth(3)
-    zb_market.get_trade()
-    zb_market.get_kline()
